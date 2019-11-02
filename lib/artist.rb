9bc20262ecd815f56f1d81d0fce53ad34bb72e9c
Karl Benbeneck <kbenbeneck@gmail.com>
2019-11-02 23:18:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  @@artists = []
+  @@artists = [] 
 
   def initialize
     @@artists << self
@@ -19,13 +19,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+#  def self.reset_all
+#    self.all.clear
+#  end
 
-  def self.count
-    self.all.count
-  end
+#  def self.count
+#    self.all.count
+#  end
 
   def add_song(song)
     @songs << song
